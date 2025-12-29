6bef48a1f565799cbb5b19a120a5f31ae242834d
Ajith <ajithpkumar231@gmail.com>
2025-12-29 22:07:54 +0530

    enhanced signin, signup page

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,30 +1,36 @@
 import { ReactElement } from "react";
 
 interface ButtonProps {
-    variant: "primary" | "secondary" | "white";
-    text?: string;
-    startIcon?: ReactElement;
-    onClick?: () => void;
-    width: "full" | "normal";
-    loading: boolean;
+	variant: "primary" | "secondary" | "white" | "temp";
+	text?: string;
+	startIcon?: ReactElement;
+	onClick?: () => void;
+	width: "full" | "normal";
+	loading: boolean;
 }
 
 const variantClasses = {
-    "primary": "bg-purple-600 text-white",
-    "secondary": "bg-purple-200 text-purple-600 font-light justify-center",
-    "white": "bg-white font-medium text-sky-600 font-light justify-center h-14"
-}
+	primary: "bg-purple-600 text-white",
+	secondary: "bg-purple-200 text-purple-600 font-light justify-center",
+	white: "bg-white font-medium text-sky-600 font-light justify-center h-14",
+	temp: "bg-blue-600 text-white",
+};
 
-const defaultStyles = `px-4 py-2 rounded-md flex items-center`
+const defaultStyles = `px-4 py-2 rounded-md flex items-center`;
 
 export const Button = (props: ButtonProps) => {
-    const { variant, text, startIcon, onClick, width, loading } = props
-    return (
-        <button className={`${variantClasses[variant]} ${defaultStyles} ${width == "full" ? "w-full flex" : ""} ${startIcon ? "justify-between" : "justify-center"} ${loading ? "opacity-60 cursor-default" : ""}`} onClick={onClick}>
-            <div className="pr-2 ">
-                {startIcon}
-            </div>
-            {text}
-        </button>
-    )
-} 
+	const { variant, text, startIcon, onClick, width, loading } = props;
+	return (
+		<button
+			className={`${variantClasses[variant]} ${defaultStyles} ${
+				width == "full" ? "w-full flex" : ""
+			} ${startIcon ? "justify-between" : "justify-center"} ${
+				loading ? "opacity-60 cursor-default" : ""
+			}`}
+			onClick={onClick}
+		>
+			<div className='pr-2 '>{startIcon}</div>
+			{text}
+		</button>
+	);
+};
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,39 +1,61 @@
-import { useRef } from "react"
-import { Button } from "../components/Button"
-import { Input } from "../components/Input"
-import axios from "axios"
-import { BACKEND_URL } from "../config"
-import { useNavigate } from "react-router-dom"
+import { useRef } from "react";
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Signup = () => {
-    const usernameRef = useRef<HTMLInputElement>()
-    const passwordRef = useRef<HTMLInputElement>()
-    const navigate = useNavigate()
-    const signUP = async () => {
-        let username = usernameRef.current?.value;
-        let password = passwordRef.current?.value;
+	const usernameRef = useRef<HTMLInputElement>();
+	const passwordRef = useRef<HTMLInputElement>();
+	const navigate = useNavigate();
+	const signUP = async () => {
+		let username = usernameRef.current?.value;
+		let password = passwordRef.current?.value;
 
-        const response = await axios.post(`${BACKEND_URL}` + "/api/v1/user/signup",
-            {
-                username,
-                password
-            }
-        )
+		const response = await axios.post(
+			`${BACKEND_URL}` + "/api/v1/user/signup",
+			{
+				username,
+				password,
+			}
+		);
 
-        if (response.status === 200) {
-            navigate("/signin")
-        }
+		if (response.status === 200) {
+			navigate("/signin");
+		}
 
-        console.log("signed up")
-    }
-    return (
-        <div className="h-screen w-screen flex justify-center items-center bg-gray-200">
-            <div className="bg-white p-10 rounded-md flex flex-col gap-3">
-                <h1 className="text-center text-lg font-semibold text-purple-600">Sign up</h1>
-                <Input compRef={usernameRef} placeholder={"username"} />
-                <Input compRef={passwordRef} placeholder={"password"} />
-                <Button variant={"primary"} text="Submit" width={"full"} loading={false} onClick={signUP} />
-            </div>
-        </div>
-    )
-}
+		console.log("signed up");
+	};
+	return (
+		<div className='h-screen w-screen flex justify-center items-center bg-gray-200'>
+			<div className='h-screen w-screen landing relative bg-gradient-to-b from-indigo-500 from-10% via-blue-600 via-30% to-blue-900 to-90%'>
+				<div className='bg-[url(/landing.jpg)] h-full w-full bg-fit absolute top-0 left-0 bg-no-repeat bg-fixed bg-cover bg-center opacity-70'></div>
+			</div>
+			<div className=' backdrop-filter backdrop-blur-md bg-opacity-15 bg-blue-400 absolute px-20 mx-auto rounded-2xl flex flex-col gap-3 py-16'>
+				<h1 className='text-center text-3xl font-semibold text-white mb-3'>
+					Sign up
+				</h1>
+				<Input compRef={usernameRef} placeholder={"username"} />
+				<Input compRef={passwordRef} placeholder={"password"} />
+				<Button
+					variant={"temp"}
+					text='Submit'
+					width={"full"}
+					loading={false}
+					onClick={signUP}
+				/>
+				<p className='text-sm text-white'>
+					Already a user?
+					<Link
+						to={"/signin"}
+						className=' hover:text-blue-500 hover:underline'
+					>
+						{" "}
+						Sign In
+					</Link>
+				</p>
+			</div>
+		</div>
+	);
+};
