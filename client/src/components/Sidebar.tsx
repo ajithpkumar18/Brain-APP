307350dbdd5bbfdbd556c5e607d731dbd1644b39
Ajith <ajithpkumar231@gmail.com>
2026-01-03 19:48:37 +0530

    Sidebar color changed

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -14,14 +14,14 @@ export const Sidebar = ({ setFilter }: { setFilter?: any }) => {
 	};
 
 	return (
-		<div className='top-0 left-0 w-96 h-screen'>
+		<div className='top-0 left-0 w-96 h-screen bg-black/95 text-white'>
 			<div className='flex p-6 items-center gap-3'>
-				<div className='text-purple-600'>
+				<div className='text-blue-600'>
 					<Logo />
 				</div>
 				<p className='text-3xl font-bold'>Brainly</p>
 			</div>
-			<div className='h-fit border-b-0 border-t-2 border-r-slate-200 pt-10'>
+			<div className='h-fit border-b-0 border-t-2 border-r-slate-200 flex flex-col gap-2 pt-8'>
 				<SidebarItem
 					text='Home'
 					icon={<HomeICon />}
--- client/src/components/SidebarItem.tsx
+++ client/src/components/SidebarItem.tsx
@@ -11,11 +11,13 @@ export const SidebarItem = ({
 }) => {
 	return (
 		<div
-			className='flex max-w-full  mx-5 px-5 rounded-xl py-4 gap-4 hover:bg-blue-200  cursor-pointer'
+			className='flex max-w-full  mx-5 px-5 rounded-xl py-4 gap-4 transition hover:bg-white/10 hover:scale-[1.05]  cursor-pointer'
 			onClick={onClick}
 		>
-			<span className=' text-blue-500 '>{icon}</span>
-			<span className='text-md font-semibold text-blue-900'>{text}</span>
+			<span className=' text-blue-500 hover:text-blue-500'>{icon}</span>
+			<span className='text-md font-semibold text-blue-600 hover:text-blue-500'>
+				{text}
+			</span>
 		</div>
 	);
 };
